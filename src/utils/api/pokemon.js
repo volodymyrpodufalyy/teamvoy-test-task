d46a24ff9fb269516b937aa710d0b8b69909157f
volodymyrpodufalyy <73552862+volodymyrpodufalyy@users.noreply.github.com>
2021-06-04 02:40:16 +0300

    Added dynamic info display for pokemon card, added detailed PokemonInfo component (#2)

--- src/utils/api/pokemon.js
+++ src/utils/api/pokemon.js
@@ -1,8 +1,8 @@
 import { axios } from "core";
 
 const pokemonApi = {
-    getAll: (offset) => axios.get(`https://pokeapi.co/api/v2/pokemon?limit=30&offset=${offset}`),
-    index: (url) => axios.get(`${url}`),
+    getAll: (pageSize) => axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${pageSize}&offset=0`),
+    getPokemonData: (url) => axios.get(`${url}`)
 };
   
 export default pokemonApi;
