bc31ec321b3d88d7e282034629c9cb22c6e261c5
volodymyrpodufalyy <73552862+volodymyrpodufalyy@users.noreply.github.com>
2021-06-07 00:38:57 +0300

    Added adaptive styling and filtering feature

--- src/components/PokemonInfo/index.jsx
+++ src/components/PokemonInfo/index.jsx
@@ -3,8 +3,18 @@ import { pokemonApi } from "utils/api";
 import "./PokemonInfo.scss";
 import { Spin } from "components"
 
-const PokemonInfo = ({ card, renderImage, renderTypes, convertToUpperCase }) => {
-    const [pokemonInfo, setPokemonInfo] = useState()
+const PokemonInfo = ({ card, renderImage, convertToUpperCase, onClose }) => {
+    const [pokemonInfo, setPokemonInfo] = useState();
+
+    const generateIndex = (id) => {
+        if(id >= 10 && id < 100) return `#0${id}`
+        if(id >= 100) {
+            return `#${id}`
+        }
+        return `#00${id}`
+    }
+
+  
 
     useEffect(() => {
         pokemonApi.getPokemonData(card.url).then(({data}) => setPokemonInfo(data));
@@ -17,18 +27,42 @@ const PokemonInfo = ({ card, renderImage, renderTypes, convertToUpperCase }) => 
     return (
         <div className="pokemon__info" >
             <div className="pokemon__info-img">
+                <button onClick={onClose} >X</button>
                 <img src={renderImage(pokemonInfo.id)} alt="pokemon"></img>
             </div>
             <div className="pokemons__info-content">
-                <h3 className="pokemon__info-content-name" >{convertToUpperCase(card.name) + ` #00${pokemonInfo.id}`}</h3>
-                {/* <div className="pokemons__list-card-info-types" >
-                    {pokeData.types.map((item, i) => {
-                        return (<p style={{ backgroundColor: `${pokemonColors[i]}` }} 
-                        key={item.slot} >
-                            {convertToUpperCase(item.type.name)}
-                        </p>);
-                    })}
-                </div> */}
+                <h3 className="pokemon__info-content-name" >{convertToUpperCase(card.name) + generateIndex(pokemonInfo.id)}</h3>
+                <div className="pokemons__info-content-table" >
+                    <table className="pokemon__table" >
+                        <thead>
+                            <tr>
+                                <th>Types</th>
+                                <th>Attack</th>
+                                <th>Defense</th>
+                                <th>HP</th>
+                                <th>SP Attack</th>
+                                <th>SP Defense</th>
+                                <th>Speed</th>
+                                <th>Weight</th>
+                                <th>Total Moves</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{pokemonInfo.types.map((item, i) => {
+                                    if (pokemonInfo.types.length === i + 1 && pokemonInfo.types.length !== 1)
+                                    return <span key={item.slot} > {', ' + convertToUpperCase(item.type.name)} </span>
+                                    return <span key={item.slot} > {convertToUpperCase(item.type.name)} </span>
+                                })}</td>
+                                {pokemonInfo.stats.map(item => (
+                                    <td key={item.stat.url} >{item.base_stat}</td>
+                                ))}
+                                <td>{pokemonInfo.weight}</td>
+                                <td>{pokemonInfo.moves.length}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
--- src/utils/api/pokemon.js
+++ src/utils/api/pokemon.js
@@ -2,7 +2,9 @@ import { axios } from "core";
 
 const pokemonApi = {
     getAll: (pageSize) => axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${pageSize}&offset=0`),
-    getPokemonData: (url) => axios.get(`${url}`)
+    getPokemonData: (url) => axios.get(`${url}`),
+    filterByType: (type) => axios.get(`https://pokeapi.co/api/v2/type/${type}`),
+    getTypes: () => axios.get(`https://pokeapi.co/api/v2/type`),
 };
   
 export default pokemonApi;
